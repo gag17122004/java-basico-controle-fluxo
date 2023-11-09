77f1684a458d1b343dc4d3f191d1a372e725f1d7
Guilherme <guianager@gmail.com>
2023-11-09 14:34:13 -0300

    terceiro commit

--- src/ExemploForArray.java
+++ src/ExemploForArray.java
@@ -2,8 +2,11 @@ public class ExemploForArray {
     public static void main(String[] args) {
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
-	for (int x=0; x<alunos.length; x++) {
-		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
-	}
+        //em arrays o indice iniciaem zero
+	    for (int x=0; x<alunos.length; x++) {
+
+	    	System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+
+	    }
     }
 }
